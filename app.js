985ba476ea5ebb00f737fe0dcd1f31528ad00b09
Sergei <s.borunov@yahoo.com>
2024-05-12 14:29:38 +0500

    1

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Hello world!\n');
+
+//const data = 'RE8xMDF4IGlzIGF3ZXNvbWUh';
+//console.log(Buffer.from(data, 'base64').toString("ascii"));
+
+
+console.log('what the f_ck?')
+console.log('what the f_ck?') 
